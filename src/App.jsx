b94771c2d3d2e547d8eaf529b84525933bee040f
Nikita Titov <nikitatitov255@gmail.com>
2024-12-02 00:03:46 +0200

    fix 0.3

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom'; // Добавляем BrowserRouter
 import LeftSide from './components/LeftSide.jsx';
 import RightSide from './components/RightSide.jsx';
 
 const App = () => {
   return (
-    <div className="container">
-      <div className="left-column">
-        <LeftSide />
+    <BrowserRouter basename="/resumejs"> {/* Оборачиваем приложение в BrowserRouter */}
+      <div className="container">
+        <div className="left-column">
+          <LeftSide />
+        </div>
+        <div className="right-column">
+          <RightSide />
+        </div>
       </div>
-      <div className="right-column">
-        <RightSide />
-      </div>
-    </div>
+    </BrowserRouter>
   );
 };
 
